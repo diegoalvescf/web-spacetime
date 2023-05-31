0088cb068bbd12ca9e941d2da1481c0246ee83b8
diegoalvescf <dalvesdocs@gmail.com>
2023-05-31 00:13:25 -0300

    feat: initial setting - auth config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(ttc|otf)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          outputPath: "static/fonts/",
+          publicPath: "_next/static/fonts/",
+        },
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
